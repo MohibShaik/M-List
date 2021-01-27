214460872e7cc69935a0b6512c2b44f49e970e8b
mohib.shaik <mohib.shaik@infomagnus.com>
2021-01-27 13:35:22 +0530

    added routing links for ion menu

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Auth } from 'aws-amplify';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -9,26 +11,42 @@ import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
 export class LoginPage implements OnInit {
 
   public form: FormGroup;
-  constructor(public router: Router , private fb: FormBuilder) { 
+  constructor(public router: Router, private fb: FormBuilder) {
     this.createForm();
   }
 
   ngOnInit() {
+    this.signIn();
   }
 
   public navigateToHomeScreen() {
-    this.router.navigate(['auth'])
+    this.router.navigate(['auth']);
   }
+
+
+
+  async signIn() {
+    try {
+      const user = await Auth.signIn('mohib.shaik16@gmail.com', 'Mohib123@');
+    } catch (error) {
+      console.log('error signing in', error);
+    }
+  }
+
 
   public createForm() {
-    this.form = this.fb.group({
-       email: ['', Validators.required ],
-       password: ['', Validators.required ]
-    });
-  }
+  this.form = this.fb.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+}
 
-  public ValidateUser(){
-    this.router.navigate(['dashboard'])
-  }
+  public ValidateUser() {
+  this.router.navigate(['dashboard']);
+}
+
+
 
 }
+
+
