d7fb86c3f868e0c7560cf54c9bc92bc1deef921a
Shahnaz Shaik <mohib.shaik16@gmail.com>
2021-07-25 22:47:25 +0530

    commented out aws amplify code

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,52 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Auth } from 'aws-amplify';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+// import { Auth } from 'aws-amplify';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  selector: "app-login",
+  templateUrl: "./login.page.html",
+  styleUrls: ["./login.page.scss"],
 })
 export class LoginPage implements OnInit {
-
   public form: FormGroup;
   constructor(public router: Router, private fb: FormBuilder) {
     this.createForm();
   }
 
-  ngOnInit() {
-    this.signIn();
-  }
+  ngOnInit() {}
 
   public navigateToHomeScreen() {
-    this.router.navigate(['auth']);
+    this.router.navigate(["auth"]);
   }
-
-
-
-  async signIn() {
-    try {
-      const user = await Auth.signIn('mohib.shaik16@gmail.com', 'Mohib123@');
-    } catch (error) {
-      console.log('error signing in', error);
-    }
-  }
-
 
   public createForm() {
-  this.form = this.fb.group({
-    email: ['', Validators.required],
-    password: ['', Validators.required]
-  });
-}
+    this.form = this.fb.group({
+      email: ["", Validators.required],
+      password: ["", Validators.required],
+    });
+  }
 
   public ValidateUser() {
-  this.router.navigate(['dashboard']);
+    this.router.navigate(["dashboard"]);
+  }
 }
-
-
-
-}
-
-
--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,20 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.page.html',
-  styleUrls: ['./register.page.scss'],
+  selector: "app-register",
+  templateUrl: "./register.page.html",
+  styleUrls: ["./register.page.scss"],
 })
 export class RegisterPage implements OnInit {
+  constructor(public router: Router) {}
 
-  constructor(public router: Router) { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  public navigateToHomeScreen() {
+    this.router.navigate(["auth"]);
   }
-
-  public navigateToHomeScreen(){
-    this.router.navigate(['auth'])
-  }
-
 }
