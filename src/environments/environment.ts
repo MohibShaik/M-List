a841b4a841f22ffa2bfa0cfcae08619f2323487c
Shahnaz Shaik <mohib.shaik16@gmail.com>
2021-08-27 16:22:14 +0530

    started integration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,5 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyAuX5Dsq3jWTQF2QxZ-6zMdxrSJf3l9xWE",
-    authDomain: "m-list-7ef87.firebaseapp.com",
-    databaseURL: "https://m-list-7ef87.firebaseio.com",
-    projectId: "m-list-7ef87",
-    storageBucket: "m-list-7ef87.appspot.com",
-    messagingSenderId: "799255526080",
-    appId: "1:799255526080:web:d9914481c2a712e3f08b25",
-    measurementId: "G-HRTELBCJ02"
-  }
+  baseURL: 'http://localhost:8081/api/',
 };
